287c6770cadee1895dafd626365908f69ab7151f
HenrySck075 <111231428+HenrySck075@users.noreply.github.com>
2022-09-29 20:58:36 +0700

    Real quick image conversion

---
+++ imgconvgui.py
@@ -0,0 +1,97 @@
+import os
+import threading
+from tkinter import *
+from tkinter import filedialog
+from tkinter import ttk
+from typing import Literal, Tuple
+from PIL import Image
+
+filetypes={
+    '*.png': "Portable Network Graphics", 
+    '*.jpg': "Joint Photographic Experts Group", 
+    '*.jgeg': "Joint Photographic Experts Group", 
+    '*.tiff': "Tagged Image File Format", 
+    '*.webp': "WebP Image Format", 
+    '*.svg': "Scalable Vector Graphics", 
+    '*.sun': "SUN Rasterfile", 
+    'apng': "Animated PNG", 
+    '*.gif': "Graphics Interchange Format", 
+    '*.bmp': "Bitmap Image", 
+    '*.psd': "Adobe Photoshop bitmap", 
+    '*.heif': "High Efficiency Image File", 
+    '*.jpe': "Joint Photographic Experts Group", 
+    '*.jfif': "Joint Photographic Experts Group", 
+    '*.jfi': "Joint Photographic Experts Group"
+}
+r=Tk()
+r.title("Light Image Conversion")
+Canvas(r, width=200, height=5).pack()
+Label(r, text="Choose an image or folder").pack()
+h=Frame(r, borderwidth=0)
+Button(h, text="Images", command=lambda x=None: o(type='Files')).pack(side=LEFT)
+Button(h, text="Folder", command=lambda x=None: o(type='Folder')).pack()
+Label(r, text="Convert from").pack()
+extfrom=StringVar()
+OptionMenu(r, extfrom, *[g.replace("*.", "") for g in filetypes]).pack()
+def o(type: Literal['Files', 'Folder']):
+    filelist=()
+    match type:
+        case "Files":
+            files=filedialog.askopenfilenames(title="Open multiple files...", parent=r, filetypes=[(filetypes[a], a) for a in filetypes.keys()])
+            filelist=files
+        case "Folder":
+            folder=filedialog.askdirectory(title="Open folder..", mustexist=True, parent=r)
+            filelist=tuple([i for i in os.listdir(folder) if os.path.isfile(os.path.join(folder, i)) and i.split(".")[-1] == extfrom])
+            os.chdir(folder)
+    Label(r, text="Files:").pack()
+    listbox=Listbox(r); listbox.pack(expand=True,fill="both")
+    scroll=Scrollbar(listbox); scroll.pack(side = RIGHT, fill = BOTH)
+    listbox.config(yscrollcommand=scroll.set)
+    scroll.config(command = listbox.yview)
+    for f in filelist:
+        listbox.insert(END, f)
+    Label(r, text="Convert to").pack()
+    extto=StringVar()
+    OptionMenu(r, extto, *[g.replace("*.", "") for g in filetypes]).pack()
+    Button(r, text="Convert", command=lambda x=None, y=None: convert(extfrom.get(), extto.get(), filelist)).pack()
+
+def convert(_from, _to, files): 
+    r.title("Processing... Please do not close this window")
+    iables=locals()
+    for c in r.winfo_children():
+        try:
+            c.configure(state="disabled")
+        except:pass
+    match=[]
+    for f in files:
+        exten = f.split(".")
+        bef=''.join([".", exten[-1]])
+        aft=f".{_to}"
+        if bef != aft and bef == f".{_from}":
+            match.append(f)
+    for shards in range(6): iables[f"list{shards+1}"]=[files[m] for m in range(len(files)) if m%6==shards]
+    for shards in range(6): threading.Thread(target=actualconvertfr, args=(_from, _to, iables[f"list{shards+1}"])).start()
+    while True:
+        if threading.active_count() == 0: 
+            for c in r.winfo_children():
+                try:
+                    c.configure(state="normal")
+                except:pass
+            r.title("Light Image Conversion")
+            break
+
+def actualconvertfr(_from, _to, files: Tuple[str]):
+    for f in files:
+        img=Image.open(f)
+        exten = f.split(".")
+        bef=''.join([".", exten[-1]])
+        aft=f".{_to}"
+        if bef != aft and bef == f".{_from}":
+            if _to in ["jpg", "jpeg", "jfif", "jfi"]:
+                im = img.convert('RGB')
+            else: im=img
+            temp=f.replace(bef, aft)
+            im.save(temp)
+            os.remove(f)
+h.pack()
+r.mainloop()
