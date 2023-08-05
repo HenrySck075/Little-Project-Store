74f44dc421e6179463f4e6df532557321ac8dc13
HenrySck075 <email.is.not.vaild9@gmail.com>
2023-08-05 23:11:16 +0700

    meow

--- vtubers.py
+++ vtubers.py
@@ -7,38 +7,51 @@ rb = lambda s: s.replace("(", "").replace(")", "")
 "Remove curved brackets"
 
 urlMap = {
-    "Twitch":"twitch.tv",
-    "Twitcasting":"twitcasting.tv",
-    "17live":"17.live",
-    "SHOWROOM":"showroom-live.com",
-    "NicoNico":"nicovideo.jp",
-    "OPENREC.tv":"openrec.tv",
-    "Mildom":"mildom.com",
-    "REALITY":"reality.app",
-    "Mixch":"mixch.tv",
-    "Mirrativ":"mirrativ.com",
-    "bilibili":"bilibili.com",
-    "fanbox":"fanbox.cc",
-    "TikTok":"tiktok.com",
-    "LINE Live":"live.line.me",
-    "YouTube":"youtube.com",
-    "Twitter":"twitter.com",
-    "Facebook":"facebook.com",
-    "Instagram":"instagram.com",
-    "Carrd":"carrd.co"
+    "Twitch":["twitch.tv"],
+    "Twitcasting":["twitcasting.tv"],
+    "17live":["17.live"],
+    "SHOWROOM":["showroom-live.com"],
+    "NicoNico":["nicovideo.jp"],
+    "OPENREC.tv":["openrec.tv"],
+    "Mildom":["mildom.com"],
+    "REALITY":["reality.app"],
+    "Mixch":["mixch.tv"],
+    "Mirrativ":["mirrativ.com"],
+    "bilibili":["bilibili.com","bilibili.tv"],
+    "fanbox":["fanbox.cc"],
+    "TikTok":["tiktok.com"],
+    "LINE Live":["live.line.me"],
+    "YouTube":["youtube.com"],
+    "Twitter":["twitter.com"],
+    "Facebook":["facebook.com"],
+    "Instagram":["instagram.com"],
+    "Carrd":["carrd.co"],
+    "Discord":["discord.com","discord.gg"],
+    "Weibo":["weibo.com"],
+    "Kick":["kick.com"],
+    "Reddit":["reddit.com"],
+    "pixiv":["pixiv.net"],
+    "Spotify":["spotify.com"],
+    "Apple Music":["music.apple.com"],
+    "DeviantArt":["deviantart.com"],
+    "Marshmallow":["marshmallow-qa.com"],
+    "SoundCloud":["soundcloud.com"],
+    "VT Social":["vt.social"],
+    "Ko-Fi":["ko-fi.com"]
 }
-def getServiceByUrl(url):
-    if all(i not in url for i in urlMap.values()):
-        print(f"An undefined service detected: {url}")
-        name = input("Please enter the service name: ")
-        host = input("Please enter host url: ")
-        urlMap[name] = host
-        return name
 
-    else:
-        for i in urlMap:
-            if urlMap[i] in url: return i
-        else: return ""
+def getServiceByUrl(url):
+    for i in urlMap.keys():
+        if any(j in url for j in urlMap[i]): return i
+
+    print(f"An undefined service detected: {url}")
+    name = input("Please enter the service name: ")
+    host = input("Please enter host url: ").split(", ")
+
+    urlMap[name] = host
+
+    return name
+
 
 
 urls = [
@@ -65,7 +78,7 @@ def minor_vtubers():
     except:vtubers=[]
     for url in urls:
         print(f"---------- Fetching {url}")
-        for idx, item in enumerate(BeautifulSoup(requests.get(url).text).select("#minor-vtuber-table tbody tr")):
+        for idx, item in enumerate(BeautifulSoup(requests.get(url).text,"lxml").select("#minor-vtuber-table tbody tr")):
             print(f"---------- Item ID: {idx}")
             items = item.select("td")
             cursed = len(items)
@@ -136,33 +149,38 @@ def major_vtubers(startFrom=None):
         apUrl+=f"?from={startFrom.replace(' ', '+').replace('/','%%2F')}&to=&namespace=0" # there's no need for urldecode bc page names are already in english
     prevPage = ""
     while True:
-        mainsoup = BeautifulSoup(requests.get(apUrl).text)
+        mainsoup = BeautifulSoup(requests.get(relativeURL(apUrl)).text,"lxml")
         anchors = mainsoup.select_one(".mw-allpages-nav").select("a")
         apUrl = anchors[-1].attrs["href"]
         if apUrl == prevPage: break
-        pages = mainsoup.select_one(".mw-allpages-chunk").select("li a")
-        for i in pages:
-            anchor = i.select_one("a")
-            if any(b in anchor.attrs["href"] for b in ["/Gallery", "/Discography", "(disambiguation)"]) or anchor.attrs.get("class",None) is not None: continue
+        pages = mainsoup.select(".mw-allpages-chunk li a")
+        for anchor in pages:
+            if any(b in anchor.text for b in ["/Gallery", "Discography", "(disambiguation)", "Historical Milestones"]) or anchor.attrs.get("class",None) is not None: continue
             print(f"---------- Fetching {relativeURL(anchor.attrs['href'])}")
-            soup = BeautifulSoup(requests.get(relativeURL(anchor.attrs["href"])).text)
+            soup = BeautifulSoup(requests.get(relativeURL(anchor.attrs["href"])).text,"lxml")
             # if this is not a page for agency, continue
             if soup.select_one("#Members") == None:
                 entry = {}
-                entry["name"] = soup.select_one(".page-header__title").text[1:-2]
+                entry["name"] = soup.select_one(".page-header__title").text[1:-1]
 
                 origName = soup.select_one("[data-source='original_name'] div")
                 entry["originalName"] = origName.text if origName != None else ""
                 extUrl = {}
-                for u in soup.select(".portable-infobox-wrapper aside section:nth-child(2) div:not(:first-child) a"):
-                    svc=getServiceByUrl(u.attrs["href"])
-                    t=u.attrs["href"]
-                    if svc in extUrl:
-                        if type(extUrl[svc]) != list:
-                            extUrl[svc] = [extUrl[svc]]
-                        extUrl[svc].append(t)
-                    else: extUrl[svc] = t
-                entry["urls"] = extUrl
+                try:
+                    for u in soup.select(".portable-infobox section")[1].select("a"):
+                        smile = u.parent
+                        if smile.name != 'div': smile = smile.parent
+                        if "Official Website" in smile.parent.select_one("h3").text: continue
+                        t=u.attrs["href"]
+                        if "#cite_note-" in t: continue
+                        svc=getServiceByUrl(u.attrs["href"])
+                        if svc in extUrl:
+                            if type(extUrl[svc]) != list:
+                                extUrl[svc] = [extUrl[svc]]
+                            extUrl[svc].append(t)
+                        else: extUrl[svc] = t
+                    entry["urls"] = extUrl
+                except IndexError:continue # https://virtualyoutuber.fandom.com/wiki/A.I._Channel or any similar wiki pages
                 vtubers.append(entry)
             else: print("^ agency page")
         prevPage = anchors[-1].attrs["href"]
@@ -174,6 +192,6 @@ except KeyboardInterrupt:
     print("Keyboard interrupted")
     json.dump(vtubers, open(resName,"w"), indent=4)
 except Exception as e:
-    traceback.print_exc(file=open("exceptionContent","w",encoding="utf-8"))
+    traceback.print_exc()
     json.dump(vtubers, open(resName,"w"), indent=4)
     
