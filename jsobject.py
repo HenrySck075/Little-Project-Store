bc593e048e6401aea70ba399d403b47c56545d3c
HenrySck075 <email.is.not.vaild9@gmail.com>
2023-08-06 22:29:35 +0700

    fix jsobject.py' Object's __repr__() function

--- jsobject.py
+++ jsobject.py
@@ -4,13 +4,13 @@ class Object(object):
         super().__init__()
         if type(d) == list:
             for i in range(len(d)):
-                try: d[i] = JSONObject(d[i])
+                try: d[i] = Object(d[i])
                 except TypeError: pass
 
         elif type(d) == dict:
             for k in d.keys():
                 v = d[k] 
-                if type(d[k]) == dict or type(d[k]) == list: v=JSONObject(d[k])
+                if type(d[k]) == dict or type(d[k]) == list: v=Object(d[k])
                 setattr(self, k, v)
         else: raise TypeError("ok")
         self.__data__ = d
@@ -18,7 +18,15 @@ class Object(object):
         self.__setattr__ = self.__setattr2__
 
     def __repr__(self):
-        return "Object {im lazy to do this sorry}"
+        hi = []
+        wrap = "[ ]"
+        if type(self.__data__) == dict:
+            wrap = "{ }"
+            for i in [n for x, n in enumerate(self.__dict__.keys()) if n in self.__data__.keys() and x < 5]: hi.append(i+": "+repr(self.__dict__[i]))
+        else:
+            hi = [repr(i) for i in self.__data__[0:5]]
+        return f"{'Object' if type(self.__data__) == dict else 'Array'} {wrap.split(' ')[0]}{', '.join(hi)}{', ...' if len(self.__data__) > 5 else ''}{wrap.split(' ')[1]}"
+    
     def __str__(self):
         return self.__repr__()
     def __getitem__(self,i):
