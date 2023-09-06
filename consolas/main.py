ce615f6d6949cc2f7b9bb440703b7ce981d207d3
HenrySck075 <email.is.not.vaild9@gmail.com>
2023-09-06 20:21:16 +0700

    m

--- consolas/main.py
+++ consolas/main.py
@@ -1,3 +1,4 @@
+import traceback, json
 import help, keys as ordkeys
 import pypatch, textwrap
 pypatch.patch()
@@ -7,25 +8,19 @@ if platform.system() == "Windows":
     print("Disconsole does not support Windows until this dumb guy switches from Textpad to curses pad (impossible)")
     raise SystemExit(1)
 
-import curses, discord, random, math, threading
-from typing import TypeVar, Generic
-from curses import textpad
+import discord, random, math, threading
+from typing import TypeVar, Generic, Callable
 import asyncio
 from rich.traceback import install
 install(show_locals=True)
-cursesWindow = curses.window
-cursesTextbox= textpad.Textbox
 
-def keygen(module):
-    return {("Shift > " + name.replace("_UP","") if "_UP" in name and name != "KEY_UP" else name):module.__dict__[name] for name in module.__dict__.keys() if "KEY_" in name}
+from prompt_toolkit.application import Application
+from prompt_toolkit.layout import Layout, Window, HSplit, VSplit, ScrollablePane, FormattedTextControl, WindowAlign, BufferControl
+from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.filters import Condition
+from prompt_toolkit.key_binding import KeyBindings, KeyPressEvent
 
 
-class keyList(list):
-    def __init__(self, iter):
-        super().__init__(iter)
-    def __getitem__(self, i):
-        if i >= self.__len__(): return -2
-        return list.__getitem__(self, i)
 _KT = TypeVar("_KT",contravariant=True)
 _VT = TypeVar("_VT",contravariant=True)
 
@@ -40,74 +35,37 @@ class DefaultDict(dict, Generic[_KT,_VT]):
         except KeyError: return self.default_value
     
     def __setitem__(self, __key: _KT, __value: _VT) -> None:
-        return super().__setitem__(__key, __value)
+        super().__setitem__(__key, __value)
+        json.dump(self, open('dict',"w"))
 # session states
-keys = keyList([])
-mode = 0 # 0 for cmd, 1 for input
+mode = 0 # 0 for cmd, 1 for input, 2 for scrolling
 scrollTarget = ""
-scrollCursorPos = DefaultDict[str,tuple[int,int]]({},(0,0))
+scrollCursorPos = DefaultDict[str,int]({},0)
 focusingG = 0
 focusingCh = 0
 guilds = []
 channels = []
 messages = []
 
-
-def fieldsValidator(i):
-    global keys
-    if i == 27:
-        keys.append(27)
-    elif i == 10 or i == curses.KEY_ENTER:
-        if not (len(keys) == 1 and keys[0] == 27):
-            return 7
-    else: keys = []
-    return i
-
-def updMode(m,target:str=""):
-    global mode
-    match m:
-        case 0:
-            windows["eventListener"].touchwin()
-        case 1:
-            if target == "msgBar":
-                textpads["msgBar"].edit(fieldsValidator)
-
-    mode = m
-class ScrlCtrl:
-    def up(self,n):
-        n[0]-=1 
-        return n
-    def down(self,n):
-        n[0]+=1 
-        return n
-    def left(self,n):
-        n[1]-=1 
-        return n
-    def right(self,n):
-        n[1]+=1 
-        return n
-scrlctrl = ScrlCtrl()
-
 def render_guilds(x=0):
     global guilds, windows
     guilds = [(i.name, i.id) for i in client.guilds]
     gwin = windows["guilds"] 
-    gwin.erase()
-    ty = 0
+    container = []
     for i in guilds:
-        gwin.addnstr(ty,0,i[0],6)
-        ty+=1
-    gwin.refresh(0,0,x,0,curses.LINES-1+x,6)
+        container.append(Window(FormattedTextControl(i[0]),width=12,height=1))
+    # gwin.refresh(0,0,x,0,curses.LINES-1+x,9)
+    gwin.content.children = container # pyright: ignore
+    app._redraw()
 
-def render_channels(gid:int, x=0):
+def render_channels(gid:int):
     global channels, windows
-    channels = [(i.type, i.name,i.id) for i in asyncio.run(client.fetch_guild(gid)).channels]
+    channels = [(i.type, i.name,i.id) for i in client.get_guild(gid).channels] # pyright: ignore
     cwin = windows["channels"]
-    cwin.erase()
-    ty = 0
+    container = []
     for i in channels:
         chIcon = ""
-        match i[0]:
+        match i[0].value:
             case 0: chIcon = "\uf4df"
             case 1: chIcon = "\uf456"
             case 2: chIcon = "\ue638"
@@ -118,98 +76,159 @@ def render_channels(gid:int, x=0):
             case 15: chIcon = "\U000f028c"
         
         if "music" in i[1]: chIcon = "\U000f02cb"
+        container.append(Window(FormattedTextControl(chIcon+" "+i[1]),width=22,height=1))
 
-        cwin.addnstr(ty,1,chIcon+" "+i[1],10)
-        ty+=1 
-    cwin.refresh(0,8,x,0,curses.LINES-5,curses.COLS-19)
+    # cwin.refresh(0,11,x,0,curses.LINES-5,curses.COLS-19)
+    cwin.content.children = container # pyright: ignore
+    app._redraw()
 
-def kbListener(evWindow: cursesWindow):
-    global scrollTarget, scrollCursorPos
-    curses.halfdelay(45)
-    while True:
-        match evWindow.getch():
-            case ordkeys.KEY_I:
-                updMode(1,"msgBar")
-                break
-            case ordkeys.KEY_Q:
-                updMode(0)
-            case ordkeys.KEY_S:
-                if len(keys) == 0: keys.append(115)
-                elif keys[0] == 27: ... # search message
-            case ordkeys.KEY_G: # g 
-                if keys[0] == 115:
-                    # scrolling server list 
-                    scrollTarget="guilds"
-            case 27: #esc
-                updMode(0)
-            case curses.KEY_ENTER | 13: #enter key (13 is carriage return)
-                if scrollTarget == "guilds":
-                    render_channels(guilds[scrollCursorPos["guilds"][1]][1])
-                    scrollTarget = "channels"
-            case curses.KEY_REFRESH:
-                windows["messages"].resize(curses.LINES-1,curses.COLS-19)
-                
+async def render_messages(cid:int):
+    global messages, windows
+    stfupyright: discord.TextChannel = client.get_channel(cid) # pyright: ignore
+    messages = [(i.id, i.author.color.__str__(), i.author.name, i.created_at, i.content) async for i in stfupyright.history(limit = 50)]
+    container = []
+    for i in messages:
+        container.insert(0, HSplit([
+            VSplit([
+                Window(FormattedTextControl(i[2],"fg:"+i[1])),
+                Window(FormattedTextControl(i[3].strftime("%m/%d/%Y, %H:%M:%S"),"fg:gray"))
+            ],height=1,padding=4,padding_char=""),
+            Window(FormattedTextControl(i[4]),wrap_lines=True)
+        ]))
+    mwin = windows["messageContent"]
+    mwin.content.children = container # pyright: ignore
+    app._redraw()
 
-            # scrolling
-            case curses.KEY_UP:
-                scrollCursorPos = help.modifyValue(scrollCursorPos,scrollTarget, scrlctrl.up)
-            case curses.KEY_DOWN:
-                scrollCursorPos = help.modifyValue(scrollCursorPos,scrollTarget, scrlctrl.down)
-            case curses.KEY_UP | curses.KEY_DOWN | curses.KEY_LEFT | curses.KEY_RIGHT:
-                windows[scrollTarget].move(*scrollCursorPos[scrollTarget])
+def keybind_lore():
+    kb = KeyBindings()
 
-conf: dict = help.loadJson("config.json") # pyright: ignore
-windows: dict[str,cursesWindow] = {}
-textpads: dict[str,cursesTextbox] = {}
-def main(stdscr):
-    global windows, colorable, client, textpads, keynames
-    messageWindows = []
+    @kb.add("s","g", filter=Condition(lambda: mode == 0))
+    def scrollGuild(e: KeyPressEvent):
+        global scrollTarget, mode
+        scrollTarget = "guilds"
+        mode = 2
+        windows[scrollTarget].content.get_children()[scrollCursorPos[scrollTarget]].style = selectHighlight # pyright: ignore
 
-    # init curses
+    @kb.add("s","c", filter=Condition(lambda: mode == 2 and scrollTarget == "channels"))
+    def scrollChannel(e: KeyPressEvent):
+        global scrollTarget, mode
+        scrollTarget = "channels"
+        mode = 2
+        st = scrollTarget+("" if scrollTarget == "guilds" else "_"+str(focusingG))
+        windows[scrollTarget].content.get_children()[scrollCursorPos[st]].style = selectHighlight # pyright: ignore
+
+    @kb.add("up", filter=Condition(lambda: scrollTarget != ""))
+    def sup(e: KeyPressEvent):
+        global focusingG
+        st = scrollTarget+("" if scrollTarget == "guilds" else "_"+str(focusingG))
+        if (i:=scrollCursorPos[st]-1) >= 0:
+            win = windows[scrollTarget].content.get_children()
+            limbo = win[i]
+            un = win[i+1]
+            un.style = "" # pyright: ignore
+            limbo.style = selectHighlight # pyright: ignore
+            scrollCursorPos[st]-=1
+            app.layout.focus(limbo)
+
+    @kb.add("down", filter=Condition(lambda: scrollTarget != ""))
+    def sdown(e: KeyPressEvent):
+        global focusingG
+        st = scrollTarget+("" if scrollTarget == "guilds" else "_"+str(focusingG))
+        if (i:=scrollCursorPos[st]+1) < len((win:=windows[scrollTarget].content.get_children())):
+            win[i].style = selectHighlight # pyright: ignore
+            win[i-1].style = "" # pyright: ignore
+            scrollCursorPos[st]+=1
+            app.layout.focus(win[i])
+
+    @kb.add("enter")
+    async def click(e):
+        global mode, scrollTarget, focusingG, focusingCh
+        if mode == 2 and scrollTarget == "guilds":
+            render_channels(guilds[scrollCursorPos[scrollTarget]][1])
+            windows[scrollTarget].content.get_children()[scrollCursorPos[scrollTarget]].style=""
+            focusingG = guilds[scrollCursorPos[scrollTarget]][1]
+            scrollTarget = "channels"
+        elif mode == 2 and scrollTarget == "channels":
+            st = scrollTarget+("" if scrollTarget == "guilds" else "_"+str(focusingG))
+            await render_messages(channels[scrollCursorPos[st]][2])
+            focusingCh = channels[scrollCursorPos[st]][2]
+            mode = 0
+            scrollTarget=""
+
+    @kb.add("escape", filter=Condition(lambda: mode != 0))
+    def ret(e):
+        global mode, scrollTarget, focusingG
+        if mode == 2:
+            windows[scrollTarget].content.get_children()[scrollCursorPos[scrollTarget]].style=""
+            if scrollTarget == "guilds":
+                mode = 0 
+                scrollTarget = ""
+            if scrollTarget == "channels": 
+                scrollTarget = "guilds"
+                focusingG = 0
+
+    @kb.add("c-q")
+    async def shut(e: KeyPressEvent):
+        e.app.exit()
+        await client.close()
+
+    return kb
+ 
+
+selectHighlight = "bg:gray fg:black"
+async def main():
+    global windows, client, app, inputBoxes
+    conf = help.loadJson("config.json")
+    windows = {
+        "guilds": ScrollablePane(HSplit([Window()],width=12),show_scrollbar=False),
+        "channels":ScrollablePane(HSplit([Window()],width=22),show_scrollbar=False),
+        "messageContent":ScrollablePane(HSplit([Window()])),
+    }
+    def t(**kwargs):
+        buf = Buffer()
+        return (Window(BufferControl(buf),**kwargs), buf)
+    inputBoxes = {
+        "messageInput": t(height = 2, wrap_lines=True),
+    }
+    windows["messages"] = HSplit([windows["messageContent"], inputBoxes["messageInput"][0]]) # pyright: ignore
     
-    curses.noecho()
-    curses.cbreak(False)
-    colorable = curses.has_colors() and curses.can_change_color()
-    if colorable:
-        curses.start_color()
+    kb = keybind_lore() 
 
-    centerX = int(curses.COLS / 2)
+    lay = Layout(HSplit([Window(),Window(FormattedTextControl("Loading Disconsole"), height=2, align=WindowAlign.CENTER)]))
+    app = Application(lay,full_screen=True,mouse_support=True, key_bindings=kb)
 
-    # splash screen
-    fact = textwrap.wrap(random.choice(help.loadJson("pasta.json")),int(curses.COLS*0.9))
-    
-    factYPos = int(curses.LINES / 2) - math.floor(len(fact) / 2)
-    
-    stdscr.addstr(factYPos - 2, centerX - 4, "Did you know....")
-
-    for idx, i in enumerate(fact):
-        stdscr.addstr(factYPos+idx,centerX-math.floor(len(i)/2),i)
-
-    stdscr.addstr(curses.LINES - 2, centerX-9, "Loading Disconsole")
-
-    stdscr.refresh()
-
-    keynames = keygen(ordkeys) | keygen(curses)
-    #setup connection
-    client = discord.Client()
+    tent = discord.Intents.all()
+    client = discord.Client(intents=tent)
 
     @client.event 
     async def on_ready():
-        windows["eventListener"] = curses.newwin(1,curses.COLS,curses.COLS-1,0)
-        windows["guilds"] = stdscr.subpad(curses.LINES,6,0,0)
-        windows["channels"] = stdscr.subpad(curses.LINES,10,0,8)
-        windows["messages"] = stdscr.subpad(curses.LINES-5,curses.COLS-21,0,20)
-        windows["msgBorder"] = stdscr.subpad(4,curses.COLS-21,curses.LINES-6,20)
-        textpads["msgBar"] = textpad.Textbox(windows["msgBorder"].subwin(2,curses.COLS-19,curses.LINES-5,21))
-        help.runmethod(list(windows.values())[1:],["scrollok(True)","idlok(True)"])
+        mainw = VSplit([
+            windows["guilds"],
+            windows["channels"],
+            windows["messages"]
+        ],padding=1,padding_char="|")
 
-        stdscr.vline(0,7,"|",curses.LINES)
-        stdscr.vline(0,19,"|",curses.LINES)
-        windows["eventListener"].touchwin()
-        windows["eventListener"].keypad(True)
+        app.layout.container = mainw
 
-    # connect
-    client.run(conf["token"])
+        app._redraw()
 
+        render_guilds()
 
-curses.wrapper(main)
+    @client.event
+    async def on_message(i: discord.Message):
+        if focusingCh == i.channel.id:
+            msg = (i.id, i.author.color.__str__(), i.author.name, i.created_at, i.content)
+            windows["messageContent"].content.children.insert(0, HSplit([
+            VSplit([
+                Window(FormattedTextControl(msg[2],"fg:"+msg[1])),
+                Window(FormattedTextControl(msg[3].strftime("%m/%d/%Y, %H:%M:%S"),"fg:gray"))
+            ],height=1,padding=4,padding_char=""),
+            Window(FormattedTextControl(msg[4]),wrap_lines=True)
+        ]))
+
+    @client.event
+    async def on_error(*no, **noo):
+        traceback.print_exc(file=open("tb","a"))
+    await asyncio.wait([asyncio.create_task(client.start(token=conf["token"])), asyncio.create_task(app.run_async())])# pyright: ignore
+
+asyncio.run(main())
